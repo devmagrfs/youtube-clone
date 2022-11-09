fce43fe0f85338f55df02bb8fbb2376b134f8279
devmagrfs <devmagrfs@gmail.com>
2022-11-10 01:19:47 +0900

    [Feat] Nav 컴포넌트 기본 틀 만들기

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	const [searchText, setSearchText] = useState('');
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log('submit');
+	};
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchText(event.target.value);
+		console.log(searchText);
+	};
+
+	return (
+		<nav>
+			<div>
+				logo
+				<span>Youtube</span>
+			</div>
+			<div>
+				<form onSubmit={handleSubmit}>
+					<input type='text' placeholder='Search...' onChange={handleChange} value={searchText} />
+					<button>검색</button>
+				</form>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
