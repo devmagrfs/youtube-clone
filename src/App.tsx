6547776603b9041a8221a70528cf923fac6f1543
devmagrfs <devmagrfs@gmail.com>
2022-11-09 13:47:21 +0900

    [Feat] 라우팅 기능 초기 세팅 만들기 및 사용할 페이지 만들어두기

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Main from './pages/Main';
+import NotFound from './pages/NotFound';
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Main />,
+		errorElement: <NotFound />,
+	},
+]);
 
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<div>hello</div>
+			<RouterProvider router={router} />
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	);
