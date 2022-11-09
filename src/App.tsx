03378804174a8b8714dc96e1eb8cb48f8596a5a0
devmagrfs <devmagrfs@gmail.com>
2022-11-09 17:27:32 +0900

    [Feat] Outlet 을 쓰기위한 기초 작업

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Main from './pages/Main';
 import NotFound from './pages/NotFound';
+import Root from './pages/Root';
+import Detail from './pages/Detail';
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <Main />,
+		element: <Root />,
 		errorElement: <NotFound />,
+		children: [
+			{ index: true, element: <Main /> },
+			{ path: '/videos', element: <Detail /> },
+		],
 	},
 ]);
 
