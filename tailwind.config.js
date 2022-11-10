8aa2fa74c1df27ea0ab529b0a0e0c7a0e1295ae8
devmagrfs <devmagrfs@gmail.com>
2022-11-10 13:47:29 +0900

    [Style] 반응형 초기 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
