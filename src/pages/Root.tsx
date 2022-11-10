cf539838b0da2d7e330903822869649672458dec
devmagrfs <devmagrfs@gmail.com>
2022-11-10 13:47:52 +0900

    [Style] 겉 div에 반응형 추가하기

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -3,8 +3,9 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 const Root = () => {
+	// flex flex-col items-center
 	return (
-		<div>
+		<div className='mx-auto my-0 text-center bg-blue-400 lg:w-[968px] lg:bg-red-400 xl:w-[1024px] xl:bg-yellow-400 2xl:w-[1280px]'>
 			<Navbar />
 			<Outlet />
 		</div>
