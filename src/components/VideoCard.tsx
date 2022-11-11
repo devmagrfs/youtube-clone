0bc2d178ad21e42fb6d51e9486576f781a5b5d56
devmagrfs <devmagrfs@gmail.com>
2022-11-11 13:28:45 +0900

    [Feat] 최신 trending 비디오 리스트 불러오기 구현

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const VideoCard = () => {
-	return <div>VideoCard</div>;
+interface VideoCardProps {
+	title: string;
+	channelTitle: string;
+	publishedAt: string;
+	thumbnails: string;
+}
+
+const VideoCard = ({ thumbnails, title, channelTitle, publishedAt }: VideoCardProps) => {
+	return (
+		<div className='flex flex-col justify-center items-center w-96'>
+			<img src={thumbnails} alt='video thumbnail' className='w-full h-full' />
+			<p>{title}</p>
+			<span>{channelTitle}</span>
+			<span>{publishedAt}</span>
+		</div>
+	);
 };
 
 export default VideoCard;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import VideoCard from '../components/VideoCard';
+
+interface TrendingVideo {
+	etag: string;
+	id: string;
+	kind: string;
+	snippet: {
+		categoryId: string;
+		channelId: string;
+		channelTitle: string;
+		defaultAudioLanguage: string;
+		description: string;
+		liveBroadcastContent: string;
+		localized: {
+			description: string;
+			title: string;
+		};
+		publishedAt: string;
+		tags: [string];
+		thumbnails: {
+			default: {
+				height: number;
+				url: string;
+				width: number;
+			};
+			high: {
+				height: number;
+				url: string;
+				width: number;
+			};
+			maxres: {
+				height: number;
+				url: string;
+				width: number;
+			};
+			medium: {
+				height: number;
+				url: string;
+				width: number;
+			};
+			standard: {
+				height: number;
+				url: string;
+				width: number;
+			};
+		};
+		title: string;
+	};
+}
+
+interface TrendingList {
+	etag: string;
+	items: TrendingVideo[];
+	kind: string;
+	nextPageToken: string;
+	pageInfo: {
+		resultsPerPage: number;
+		totalResults: number;
+	};
+}
 
 const Main = () => {
-	return <div>Main</div>;
+	const [trending, setTrending] = useState<TrendingVideo[]>([]);
+	const getTrendingList = async () => {
+		const trendingList = await axios.get('data/trending.json');
+		console.log(trendingList.data.items);
+		setTrending(trendingList.data.items);
+	};
+
+	useEffect(() => {
+		getTrendingList();
+	}, []);
+	return (
+		<main className='flex flex-wrap'>
+			{trending.map((video) => {
+				return <VideoCard title={video.snippet.title} channelTitle={video.snippet.channelTitle} publishedAt={video.snippet.publishedAt} thumbnails={video.snippet.thumbnails.default.url} />;
+			})}
+		</main>
+	);
 };
 
 export default Main;
