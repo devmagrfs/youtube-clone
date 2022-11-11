e0441dbdb34fbbc9193746b03bf791f78c873c98
devmagrfs <devmagrfs@gmail.com>
2022-11-11 09:44:52 +0900

    [Style] 배경색 삭제

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -5,7 +5,7 @@ import Navbar from '../components/Navbar';
 const Root = () => {
 	// flex flex-col items-center
 	return (
-		<div className='mx-auto my-0 text-center bg-blue-400 lg:w-[968px] lg:bg-red-400 xl:w-[1024px] xl:bg-yellow-400 2xl:w-[1280px]'>
+		<div className='mx-auto my-0 text-center lg:w-[968px] xl:w-[1024px] 2xl:w-[1280px]'>
 			<Navbar />
 			<Outlet />
 		</div>
