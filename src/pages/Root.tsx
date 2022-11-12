3957514c633c247868592bbfb0257b8c754e58f4
devmagrfs <devmagrfs@gmail.com>
2022-11-12 22:35:35 +0900

    [Refactor] 시멘틱하게 컴포넌트 이름 수정

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import Header from '../components/Header';
 
 const Root = () => {
 	// flex flex-col items-center
 	return (
 		<div className='mx-auto my-0 text-center lg:w-[968px] xl:w-[1024px] 2xl:w-[1280px]'>
-			<Navbar />
+			<Header />
 			<Outlet />
 		</div>
 	);
