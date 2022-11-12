efcc3a0ad7c3275700ba3441d1275e109347eac5
devmagrfs <devmagrfs@gmail.com>
2022-11-13 01:38:53 +0900

    [Refactor] 리액트쿼리 적용 위치 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Main from './pages/Main';
 import NotFound from './pages/NotFound';
 import Root from './pages/Root';
 import Detail from './pages/Detail';
-
-const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
 	{
@@ -25,12 +21,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-	return (
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Header from '../components/Header';
 
+const queryClient = new QueryClient();
+
 const Root = () => {
-	// flex flex-col items-center
 	return (
-		<div className='mx-auto my-0 text-center lg:w-[968px] xl:w-[1024px] 2xl:w-[1280px]'>
+		<>
 			<Header />
-			<Outlet />
-		</div>
+			<QueryClientProvider client={queryClient}>
+				<Outlet />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
+		</>
 	);
 };
 
