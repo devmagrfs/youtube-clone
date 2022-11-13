3c56e969fd00b5e1194a8badcccedfe542c9980f
devmagrfs <devmagrfs@gmail.com>
2022-11-14 02:17:46 +0900

    [Feat] Main에서 사용하는 useQuery에 stale 타임 설정하기

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -67,7 +67,7 @@ export interface TrendingList {
 const Main = () => {
 	const { keyword } = useParams();
 	const { youtube } = useYoutubeApi();
-	const { isLoading, error, data: videos } = useQuery(['videos', keyword], () => youtube.search(keyword));
+	const { isLoading, error, data: videos } = useQuery(['videos', keyword], () => youtube.search(keyword), { staleTime: 1000 * 60 * 1 });
 
 	return (
 		<>
