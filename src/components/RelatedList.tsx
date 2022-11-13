6fc9c940e159630dfb112ce9946ea26e9117f43d
devmagrfs <devmagrfs@gmail.com>
2022-11-14 02:19:21 +0900

    [Feat] 관련된 비디오 보여주기 구현

--- src/components/RelatedList.tsx
+++ src/components/RelatedList.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+import VideoCard from './VideoCard';
+import { VideoType } from '../pages/Main';
 
-const RelatedList = () => {
-	return <div>RelatedList</div>;
+interface RelatedListProp {
+	id: string;
+}
+
+const RelatedList = ({ id }: RelatedListProp) => {
+	const { youtube } = useYoutubeApi();
+	const { isLoading, error, data: videos } = useQuery(['related', id], () => youtube.relatedVideos(id), { staleTime: 1000 * 60 * 5 });
+	return (
+		<>
+			{isLoading && <p>Loading...</p>}
+			{error && <p>Something is wrong...</p>}
+			{videos && (
+				<ul>
+					{videos.map((video: VideoType) => (
+						<VideoCard video={video} key={video.id} type='list' />
+					))}
+				</ul>
+			)}
+		</>
+	);
 };
 
 export default RelatedList;
