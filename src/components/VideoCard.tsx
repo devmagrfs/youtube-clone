56bc8e785097ebde22b845d55bf9b7054b753efa
devmagrfs <devmagrfs@gmail.com>
2022-11-14 02:18:48 +0900

    [Feat] 리스트냐 아니냐에 따라 스타일링 변경 구현

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -3,16 +3,22 @@ import { useNavigate } from 'react-router-dom';
 import { VideoType } from '../pages/Main';
 import { formatAgo } from '../util/date';
 
-const VideoCard = ({ video }: { video: VideoType }) => {
+interface VideoCardProp {
+	video: VideoType;
+	type?: string;
+}
+
+const VideoCard = ({ video, type }: VideoCardProp) => {
 	const { thumbnails, title, channelTitle, publishedAt } = video.snippet;
 	const navigate = useNavigate();
 	const moveDetailVideoPage = () => {
-		navigate(`/video/watch/${video.id}`, { state: { video } });
+		navigate(`/videos/watch/${video.id}`, { state: { video } });
 	};
+	const isList = type === 'list';
 
 	return (
-		<li onClick={moveDetailVideoPage}>
-			<img src={thumbnails.medium.url} alt={title} className='w-full' />
+		<li className={isList ? 'flex gap-1 m-2' : ''} onClick={moveDetailVideoPage}>
+			<img src={thumbnails.medium.url} alt={title} className={isList ? 'w-60 mr-2' : 'w-full'} />
 			<div>
 				<p className='font-semibold my-2 line-clamp-2'>{title}</p>
 				<p className='text-sm opacity-80'>{channelTitle}</p>
