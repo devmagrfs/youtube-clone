e50954f1ebbd5dfeb91fa6db3967e0868e501f68
devmagrfs <devmagrfs@gmail.com>
2022-11-14 01:22:24 +0900

    [Feat] 디테일 페이지 골격 만들기

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedList from '../components/RelatedList';
 
 const Detail = () => {
-	return <div>Detail</div>;
+	const {
+		state: { video },
+	} = useLocation();
+
+	const { title, channelId, channelTitle, description } = video.snippet;
+
+	return (
+		<section>
+			<article>
+				<iframe id='player' data-type='text/html' width='100%' height='640' src={`http://www.youtube.com/embed/${video.id}`} frameBorder='0' />
+				<div>
+					<h2>{title}</h2>
+					<ChannelInfo id={channelId} name={channelTitle} />
+					<pre>{description}</pre>
+				</div>
+			</article>
+			<section>
+				<RelatedList id={video.id} />
+			</section>
+		</section>
+	);
 };
 
 export default Detail;
