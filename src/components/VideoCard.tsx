8aa6854c5eb05b4e0beb99b52046dd7eeadf427a
devmagrfs <devmagrfs@gmail.com>
2022-11-14 00:18:43 +0900

    [Feat] 비디오카드 내용물 보여주기 구현

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { VideoType } from '../pages/Main';
+import { formatAgo } from '../util/date';
 
-interface VideoProps {
-	video: VideoType;
-}
-
-const VideoCard = ({ video }: VideoProps) => {
+const VideoCard = ({ video }: { video: VideoType }) => {
 	const { thumbnails, title, channelTitle, publishedAt } = video.snippet;
 	const navigate = useNavigate();
-
-	const moveDetailMoviePage = () => {
-		navigate(`/watch/${video.id}`);
+	const moveDetailVideoPage = () => {
+		navigate(`/video/watch/${video.id}`);
 	};
 
 	return (
-		<div className='flex flex-col justify-center items-center w-96 cursor-pointer' onClick={moveDetailMoviePage}>
-			<img src={thumbnails.default.url} alt='video thumbnail' className='w-full h-full' />
-			<p>{title}</p>
-			<span>{channelTitle}</span>
-			<span>{publishedAt}</span>
-		</div>
+		<li onClick={moveDetailVideoPage}>
+			<img src={thumbnails.medium.url} alt={title} />
+			<div>
+				<p>{title}</p>
+				<p>{channelTitle}</p>
+				<p>{formatAgo(publishedAt, 'ko')}</p>
+			</div>
+		</li>
 	);
 };
 
