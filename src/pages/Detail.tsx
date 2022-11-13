8dd2d080eb36da5b2ef5b711bf06df3205b5f455
devmagrfs <devmagrfs@gmail.com>
2022-11-14 02:17:11 +0900

    [Style] 디테일 페이지 스타일링

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -7,20 +7,21 @@ const Detail = () => {
 	const {
 		state: { video },
 	} = useLocation();
+	console.log(';;;');
 
 	const { title, channelId, channelTitle, description } = video.snippet;
 
 	return (
-		<section>
-			<article>
-				<iframe id='player' data-type='text/html' width='100%' height='640' src={`http://www.youtube.com/embed/${video.id}`} frameBorder='0' />
-				<div>
-					<h2>{title}</h2>
+		<section className='flex flex-col lg:flex-row'>
+			<article className='basis-4/6'>
+				<iframe id='player' data-type='text/html' width='100%' height='640' src={`http://www.youtube.com/embed/${video.id}`} frameBorder='0' title={title} />
+				<div className='p-8'>
+					<h2 className='text-xl font-bold'>{title}</h2>
 					<ChannelInfo id={channelId} name={channelTitle} />
-					<pre>{description}</pre>
+					<pre className='whitespace-pre-wrap'>{description}</pre>
 				</div>
 			</article>
-			<section>
+			<section className='basis-2/6'>
 				<RelatedList id={video.id} />
 			</section>
 		</section>
