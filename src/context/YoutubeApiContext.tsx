b5f7e954638d67a3f2055c7a806eca39a0fe614a
devmagrfs <devmagrfs@gmail.com>
2022-11-16 17:38:26 +0900

    [Refactor] 배포 버젼 코드로 수정

--- src/context/YoutubeApiContext.tsx
+++ src/context/YoutubeApiContext.tsx
@@ -1,12 +1,10 @@
 import { createContext, useContext } from 'react';
-import FakeYoutubeClient from '../api/fakeYoutubeClient';
 import YoutubeClient from '../api/youtubeClient';
 import Youtube from '../api/youtube';
 
 export const YoutubeApiContext = createContext<any | undefined>(undefined);
 
-const client = new FakeYoutubeClient();
-// const client = new YoutubeClient();
+const client = new YoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }: { children: React.ReactNode }) {
