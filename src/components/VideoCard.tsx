4194a91700c05b3abdb14af6cb40a0bc5a1a54eb
devmagrfs <devmagrfs@gmail.com>
2022-11-16 12:45:28 +0900

    [Refactor] 변수명에 맞게 코드 변경

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -11,7 +11,7 @@ interface VideoCardProp {
 
 const VideoCard = ({ video, type }: VideoCardProp) => {
 	const { thumbnails, title, channelTitle, publishedAt } = video.snippet;
-	const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 	const navigate = useNavigate();
 	const moveDetailVideoPage = () => {
 		navigate(`/videos/watch/${video.id}`, { state: { video } });
@@ -19,12 +19,12 @@ const VideoCard = ({ video, type }: VideoCardProp) => {
 	const isList = type === 'list';
 
 	const handleImageLoaded = () => {
-		setIsLoading(true);
+		setIsLoading(false);
 	};
 
 	return (
 		<li className={isList ? 'flex gap-1 mx-2 mb-2' : 'mx-2 mb-2'} onClick={moveDetailVideoPage}>
-			<img src={thumbnails.medium.url} alt={title} onLoad={handleImageLoaded} className={`rounded ${isList ? 'w-60 mr-2' : 'w-full'} ${isLoading ? '' : 'bg-slate-600 w-full h-[180px]'}`} />
+			<img src={thumbnails.medium.url} alt={title} onLoad={handleImageLoaded} className={`rounded ${isList ? 'w-60 mr-2' : 'w-full'} ${isLoading ? 'bg-slate-600 w-full h-[180px]' : ''}`} />
 			<div>
 				<p className='font-semibold my-2 line-clamp-2'>{title}</p>
 				<p className='text-sm opacity-80'>{channelTitle}</p>
