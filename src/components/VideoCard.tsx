8c814ee7f6890d6f497edadb1d533124c45b607a
devmagrfs <devmagrfs@gmail.com>
2022-11-17 09:55:45 +0900

    [Style] 커서 포인터 추가

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -23,7 +23,7 @@ const VideoCard = ({ video, type }: VideoCardProp) => {
 	};
 
 	return (
-		<li className={isList ? 'flex gap-1 mx-2 mb-2' : 'mx-2 mb-2'} onClick={moveDetailVideoPage}>
+		<li className={`cursor-pointer ${isList ? 'flex gap-1 mx-2 mb-2' : 'mx-2 mb-2'}`} onClick={moveDetailVideoPage}>
 			<img src={thumbnails.medium.url} alt={title} onLoad={handleImageLoaded} className={`rounded ${isList ? 'w-60 mr-2' : 'w-full'} ${isLoading ? 'bg-slate-600 w-full h-[180px]' : ''}`} />
 			<div>
 				<p className='font-semibold my-2 line-clamp-2'>{title}</p>
