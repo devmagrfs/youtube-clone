af8932d684f86362af734e69a1f15527ded3e1cc
devmagrfs <devmagrfs@gmail.com>
2022-11-19 02:22:22 +0900

    [Style] 스켈레톤 넓이 flex-grow로 수정 및 높이에 px가 아니라 rem 사용

--- src/components/VideoCardSkeleton.tsx
+++ src/components/VideoCardSkeleton.tsx
@@ -7,8 +7,8 @@ interface VideoCardSkeletonProp {
 const VideoCardSkeleton = ({ isList }: VideoCardSkeletonProp) => {
 	return (
 		<li className={`${isList ? 'flex gap-1 mx-2 mb-2' : 'mx-2 mb-2'} animate-pulse`}>
-			<div className={`${isList ? 'mr-2' : ''} w-full h-[180px] bg-slate-600`} />
-			<div className='w-full'>
+			<div className={`grow ${isList ? 'mr-2 h-36' : 'h-40'} bg-slate-600`} />
+			<div className={`${isList ? 'grow' : ''}`}>
 				<p className='h-[20px] my-2 bg-slate-600' />
 				<p className='h-[20px] my-2 bg-slate-600' />
 				<p className='h-[20px] bg-slate-600' />
