7230cd19dacb629f85211cd4b7990b0b1f666297
devmagrfs <devmagrfs@gmail.com>
2022-11-18 17:40:09 +0900

    [Feat] api에 따라 달라지는 비디오 리스트 타입, 채널 타입 추가

--- src/types/youtube.ts
+++ src/types/youtube.ts
@@ -1,7 +1,7 @@
 export interface VideoType {
-	etag?: string;
+	etag: string;
 	id: string;
-	kind?: string;
+	kind: string;
 	snippet: {
 		categoryId: string;
 		channelId: string;
@@ -46,7 +46,7 @@ export interface VideoType {
 	};
 }
 
-export interface TrendingList {
+export interface TrendingListType {
 	etag: string;
 	items: VideoType[];
 	kind: string;
@@ -56,4 +56,111 @@ export interface TrendingList {
 		totalResults: number;
 	};
 	regionCode?: string;
+}
+
+export interface RelatedVideoType {
+	etag: string;
+	id: {
+		kind: string;
+		videoId: string;
+	};
+	kind: string;
+	snippet: {
+		categoryId: string;
+		channelId: string;
+		channelTitle: string;
+		defaultAudioLanguage: string;
+		description: string;
+		liveBroadcastContent: string;
+		localized: {
+			description: string;
+			title: string;
+		};
+		publishedAt: string;
+		tags: [string];
+		thumbnails: {
+			default: {
+				height: number;
+				url: string;
+				width: number;
+			};
+			high: {
+				height: number;
+				url: string;
+				width: number;
+			};
+			maxres: {
+				height: number;
+				url: string;
+				width: number;
+			};
+			medium: {
+				height: number;
+				url: string;
+				width: number;
+			};
+			standard: {
+				height: number;
+				url: string;
+				width: number;
+			};
+		};
+		title: string;
+	};
+}
+
+export interface RelatedVideoListType {
+	kind: string;
+	etag: string;
+	nextPageToken: string;
+	regionCode: string;
+	pageInfo: {
+		totalResults: number;
+		resultsPerPage: number;
+	};
+	items: RelatedVideoType[];
+}
+
+export interface ChannelDetailType {
+	kind: string;
+	etag: string;
+	pageInfo: {
+		totalResults: number;
+		resultsPerPage: number;
+	};
+	items: [
+		{
+			kind: string;
+			etag: string;
+			id: string;
+			snippet: {
+				title: string;
+				description: string;
+				customUrl: string;
+				publishedAt: string;
+				thumbnails: {
+					default: {
+						url: string;
+						width: number;
+						height: number;
+					};
+					medium: {
+						url: string;
+						width: number;
+						height: number;
+					};
+					high: {
+						url: string;
+						width: number;
+						height: number;
+					};
+				};
+				localized: {
+					title: string;
+					description: string;
+					country: string;
+				};
+			};
+		}
+	];
 }
