6b4a480d4757b398e0fa79e97d1cd0b348dcdf1b
devmagrfs <devmagrfs@gmail.com>
2022-11-19 23:49:57 +0900

    [Feat] 이미지 로딩이 덜 되었을 시, 스켈레톤이 보이게 변경

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { VideoType } from '../types/youtube';
 import { formatAgo } from '../util/date';
+import VideoCardSkeleton from './VideoCardSkeleton';
 
 interface VideoCardProp {
 	video: VideoType;
@@ -22,27 +23,30 @@ const VideoCard = ({ video, type }: VideoCardProp) => {
 	};
 
 	return (
-		<li
-			onClick={moveDetailVideoPage}
-			className={`cursor-pointer 
+		<>
+			{isImageLoading && <VideoCardSkeleton isList={isList} />}
+			<li
+				onClick={moveDetailVideoPage}
+				className={`cursor-pointer 
         ${isList ? 'flex gap-1 mx-2 mb-2' : 'mx-2 mb-2'}
         ${isImageLoading ? 'hidden' : ''}
       `}
-		>
-			<img
-				src={thumbnails.medium.url}
-				alt={title}
-				onLoad={handleImageLoaded}
-				className={`rounded 
+			>
+				<img
+					src={thumbnails.medium.url}
+					alt={title}
+					onLoad={handleImageLoaded}
+					className={`rounded 
         ${isList ? 'w-60 mr-2' : 'w-full'} 
       `}
-			/>
-			<div>
-				<p className='font-semibold my-2 line-clamp-2'>{title}</p>
-				<p className='text-sm opacity-80'>{channelTitle}</p>
-				<p className='text-sm opacity-80'>{formatAgo(publishedAt, 'ko')}</p>
-			</div>
-		</li>
+				/>
+				<div>
+					<p className='font-semibold my-2 line-clamp-2'>{title}</p>
+					<p className='text-sm opacity-80'>{channelTitle}</p>
+					<p className='text-sm opacity-80'>{formatAgo(publishedAt, 'ko')}</p>
+				</div>
+			</li>
+		</>
 	);
 };
 
