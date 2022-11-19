9efe6fdfa6f3414f4ab1766e3bbfa6223f8078f6
devmagrfs <devmagrfs@gmail.com>
2022-11-19 23:49:15 +0900

    [Feat] 비디오 부분 스켈레톤 UI 추가

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import ChannelInfo from '../components/ChannelInfo';
+import DetailSkeleton from '../components/DetailSkeleton';
 import RelatedList from '../components/RelatedList';
 
 const Detail = () => {
 	const {
 		state: { video },
 	} = useLocation();
-	const [isLoading, setIsLoading] = useState(true);
+	const [isVideoLoading, setIsVideoLoading] = useState(true);
 
 	const { title, channelId, channelTitle, description } = video.snippet;
 
 	const handleVideoLoaded = () => {
-		setIsLoading(false);
+		setIsVideoLoading(false);
 	};
 
 	return (
 		<section className='flex flex-col lg:flex-row'>
-			<article className={`basis-4/6 ${isLoading ? 'hidden' : ''}`}>
+			{isVideoLoading && <DetailSkeleton />}
+			<article className={`basis-4/6 ${isVideoLoading ? 'hidden' : ''}`}>
 				<iframe id='player' data-type='text/html' width='100%' height='640' src={`https://www.youtube.com/embed/${video.id}`} frameBorder='0' title={title} onLoad={handleVideoLoaded} />
 				<div className='p-8'>
 					<h2 className='text-xl font-bold'>{title}</h2>
